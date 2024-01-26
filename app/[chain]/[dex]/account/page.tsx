d97b01ee89003d0af406eb83c8b737b83b93fcfe
nguyenlejoe <joe_le.1@hotmail.com>
2024-01-26 08:07:47 -0800

    style fix & wallet input

--- app/[chain]/[dex]/account/page.tsx
+++ app/[chain]/[dex]/account/page.tsx
@@ -1,25 +1,50 @@
 'use client'
-import { Chain } from "@covalenthq/client-sdk"
 import { useRouter } from "next/navigation";
 import { XYKWalletTransactionsListView, XYKWalletPoolListView  } from "@covalenthq/goldrush-kit";
 import { Flex } from "@radix-ui/themes";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Account({ params }: { params: { chain: string, dex: string } }) {
   const router = useRouter();
+  const [walletAddress, setAddress] = useState("")
+  const [input, setInput] = useState("");
 
   return (
     <div className="w-full flex flex-col gap-4">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Account
         </h1>
+        <Flex align="end" gap="4">
+          <div>
+            <Label htmlFor="wallet_address">Wallet address</Label>
+            <Input
+              className="w-[400px]"
+              type="input"
+              id="address"
+              placeholder="Wallet address"
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value)
+              }}
+            />
+          </div>
+          <Button onClick={()=>{
+            setAddress(input)
+          }}>Load account details</Button>
+        </Flex>
         <h2 className="text-xl font-extrabold leading-tight tracking-tighter md:text-2xl">
           Pools
         </h2>
         <XYKWalletPoolListView
           chain_name={params.chain}
           dex_name={params.dex}
-          wallet_address={"0xfC43f5F9dd45258b3AFf31Bdbe6561D97e8B71de"}
+          wallet_address={walletAddress}
+          on_pool_click={(e: any)=>{
+            router.push(`/${params.chain}/${params.dex}/pools/${e}`)
+          }}
         />
         <h2 className="text-xl font-extrabold leading-tight tracking-tighter md:text-2xl">
           Transactions
@@ -27,7 +52,7 @@ export default function Account({ params }: { params: { chain: string, dex: stri
         <XYKWalletTransactionsListView
           chain_name={params.chain}
           dex_name={params.dex}
-          wallet_address={"0xfC43f5F9dd45258b3AFf31Bdbe6561D97e8B71de"}
+          wallet_address={walletAddress}
         />
         <Flex onClick={()=>{
           router.back()
